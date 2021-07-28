db2bcb5008a0792fe36e4a8dd929f3d4bfd071b1
hens2013 <hens2013@gmail.com>
2021-07-28 12:33:47 +0300

    update

--- actions.py
+++ actions.py
@@ -1,24 +1,7 @@
-import re
-import io
-import ast
-import requests
-import numpy as np
-import pandas as pd
-import random
 from typing import Any, Text, Dict, List, Union, Optional
 from rasa_sdk import Action, Tracker
-from rasa_sdk import FormValidationAction
-from rasa_sdk.events import SlotSet, FollowupAction
-from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
-import warnings
-from statistics import mean
-from os import path, getenv
-from datetime import datetime
-import matplotlib.pyplot as plt
-from botocore.exceptions import ClientError
-from boto3.exceptions import S3UploadFailedError
-import boto3
+
 
 class FirstAction(Action):
     def name(self) -> Text:
