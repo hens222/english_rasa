e081d4d6a8977cc3c419e10041eb61523a586bf1
hens2013 <hens2013@gmail.com>
2021-07-28 12:10:38 +0300

    Initial commit

---
+++ actions.py
@@ -0,0 +1,14 @@
+from typing import Any, Text, Dict, List, Union, Optional
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class FirstAction(Action):
+    def name(self) -> Text:
+        return "action_first"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(test="I am the first action")
+        return []
